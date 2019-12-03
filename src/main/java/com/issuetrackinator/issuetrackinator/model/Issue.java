5f73a1663e024e97478543fa8db2fcb5108e618a
marc.revelles <marc.revelles@est.fib.upc.edu>
2019-12-04 00:54:40 +0100

    HOT FIX: Missing import from merge conflict resolve

--- src/main/java/com/issuetrackinator/issuetrackinator/model/Issue.java
+++ src/main/java/com/issuetrackinator/issuetrackinator/model/Issue.java
@@ -8,6 +8,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 @Entity
