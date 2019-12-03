4d89d1583751c64ad25d00c5d9fc517659ec4ab7
marc.revelles <marc.revelles@est.fib.upc.edu>
2019-12-03 06:39:11 +0100

    Add authorization

--- src/main/java/com/issuetrackinator/issuetrackinator/config/SwaggerConfig.java
+++ src/main/java/com/issuetrackinator/issuetrackinator/config/SwaggerConfig.java
@@ -6,33 +6,37 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newHashSet;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket api() {
-        Set<String> protocols = new HashSet<>();
-        protocols.add("http");
-
-        Set<String> mimeTypes = new HashSet<>();
-        mimeTypes.add("application/json");
-
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.issuetrackinator.issuetrackinator"))
                 .paths(PathSelectors.any())
                 .build()
                 .host("localhost:8080")
-                .protocols(protocols)
-                .produces(mimeTypes)
-                .consumes(mimeTypes)
+                .securitySchemes(newArrayList(apiKey()))
+                .securityContexts(newArrayList(securityContext()))
+                .protocols(newHashSet("http"))
+                .produces(newHashSet("application/json"))
+                .consumes(newHashSet("application/json"))
                 .apiInfo(getApiInfo());
     }
 
@@ -42,5 +46,20 @@ public class SwaggerConfig {
                 .description("Swagger documentation to test our issue tracker API.")
                 .version("1.0.0")
                 .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("ApiKeySchema", "api_key", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        return newArrayList(new SecurityReference("ApiKeySchema", new AuthorizationScope[0]));
     }
 }
