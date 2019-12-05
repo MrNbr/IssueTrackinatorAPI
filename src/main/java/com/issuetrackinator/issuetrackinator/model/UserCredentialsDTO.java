ff4321bc7dd9e6a7e8e4877073a092eb62b2863a
marc.revelles <marc.revelles@est.fib.upc.edu>
2019-12-05 02:07:19 +0100

    Document UserCredentialsDTO

--- src/main/java/com/issuetrackinator/issuetrackinator/model/UserCredentialsDTO.java
+++ src/main/java/com/issuetrackinator/issuetrackinator/model/UserCredentialsDTO.java
@@ -1,14 +1,20 @@
 package com.issuetrackinator.issuetrackinator.model;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class UserCredentialsDTO
 {
 
+    @ApiModelProperty(example = "tom", position = 0)
     private String username;
 
+    @ApiModelProperty(example = "email@provider.com", position = 1)
     private String email;
 
+    @ApiModelProperty(example = "password123", position = 2)
     private String password;
 
+    
     public String getUsername()
     {
         return username;
